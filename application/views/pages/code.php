aeca61670df8e12f11c8f2c20d88a19afd4b73a7
herosolutions94 <herosolutions.tk@gmail.com>
2024-05-01 12:03:21 +0500

    merging finished

--- application/views/pages/code.php
+++ application/views/pages/code.php
@@ -90,47 +90,22 @@
                 </div>
                 <?php endforeach ?>
 
-                <div class="bottom_content">
-                    <div class="image_2">
-                        <img src="<?= base_url() . 'uploads/images/' . $site_settings->site_logo . '?v-' . $site_settings->site_version ?>"
-                            alt="<?= $site_settings->site_name ?>" alt="">
-                    </div>
-                    <div class="content">
-                        <h5><a
-                                href="<?= base_url('code-detail/' . urlencode(doEncode($code_p->id))) ?>"><?= $code_p->title ?></a>
-                        </h5>
-                        <p><?= $code_p->code ?></p>
-                        <div class="cta_price">
-                            <h5><strong><?= format_amount(number_format($code_p->total_price, 2, '.', '')) ?></strong>
-                            </h5>
-                            <div class="cta">
-                                <a href="javascript:void(0)" class="webBtn popBtn"
-                                    data-lot_type="<?= ($code_p->type) ?>" data-inventory_id="<?= ($code_p->id) ?>"
-                                    data-popup="add_code">Add</a>
-                            </div>
-                        </div>
+
+
+                <?php else : ?>
+
+                <div class="alert alert-danger">No Code Available!!!</div>
+
+                <?php endif; ?>
+
+            </div>
+            <div id="loading_codes" class="hidden">
+                <div class="loadingio-spinner-rolling-2by998twmg8">
+                    <div class="ldio-yzaezf3dcmj">
+                        <div></div>
                     </div>
                 </div>
             </div>
-        </div>
-        <?php endforeach ?>
-
-
-
-        <?php else : ?>
-
-        <div class="alert alert-danger">No Code Available!!!</div>
-
-        <?php endif; ?>
-
-        </div>
-        <div id="loading_codes" class="hidden">
-            <div class="loadingio-spinner-rolling-2by998twmg8">
-                <div class="ldio-yzaezf3dcmj">
-                    <div></div>
-                </div>
-            </div>
-        </div>
 
         </div>
     </section>
